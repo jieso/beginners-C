2a84c1a1683982ed369692915b3f977c987cc26a
jieso <114213444+jieso@users.noreply.github.com>
2023-04-03 22:49:03 +0800

    Add files via upload

---
+++ zyaf.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<assert.h>
+#include<math.h>
+#include<stdbool.h>
+
+void menu(void);
+
+int main()
+{
+	void menu(void);
+	return 0;
+}
+
+
+//void AdJustDown(int* a, int n, int parent)
+//{
+//	while (parent<n)
+//	{
+//		if (a[(parent * 2) + 1] < a[(parent * 2) + 2])
+//		{
+//			if (a[(parent * 2) + 1] < a[parent])
+//			{
+//				Swap(&a[(parent * 2) + 1], &a[parent]);
+//				parent = parent * 2 + 1;
+//			}
+//			else
+//				return;
+//		}
+//		else
+//		{
+//			if (a[(parent * 2) + 2] < a[parent])
+//			{
+//				Swap(&a[(parent * 2) + 2], &a[parent]);
+//				parent = parent * 2 + 2;
+//			}
+//			else
+//				return;
+//		}
+//	}
+//}
